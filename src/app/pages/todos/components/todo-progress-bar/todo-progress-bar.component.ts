82dbc455df3c9bb8ca29133d4ae399db53d36ebf
Alexis Aravena / InGe! <aaravena@inge.cl>
2024-11-08 12:58:22 -0300

    Added progress to visualize distribution of completed/working todos

--- src/app/pages/todos/components/todo-progress-bar/todo-progress-bar.component.ts
+++ src/app/pages/todos/components/todo-progress-bar/todo-progress-bar.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TodoStoreSummary } from '../../../../store/todos/todo.model';
+import { select, Store } from '@ngrx/store';
+import { selectTodosSummary } from '../../../../store/todos/todo.selector';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'todo-progress-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-progress-bar.component.html',
   styleUrl: './todo-progress-bar.component.scss'
 })
-export class TodoProgressBarComponent {
+export class TodoProgressBarComponent implements OnInit{
+  public summary$: Observable<TodoStoreSummary>;
+  public todoPercentage: number = 0;
+  public completedPercentage: number = 0;
 
+  constructor(private store: Store) {
+    this.summary$ = this.store.pipe(select(selectTodosSummary));
+  }
+
+  public calculatePercentageDistro(value: number, total: number) {
+    if(total === 0) return 0;
+    return  (value / total) * 100;
+  }
+
+  ngOnInit(): void {
+  }
 }
